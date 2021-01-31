909eed0db6040fd7c679a0709d43e18fa51946fc
张亚飞 <672233580@qq.com>
2021-01-31 20:38:01 +0800

    Initial Commit

---
+++ Parallax Carousel/Parallax_CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  Parallax_CarouselApp.swift
+//  Parallax Carousel
+//
+//  Created by 张亚飞 on 2021/1/31.
+//
+
+import SwiftUI
+
+@main
+struct Parallax_CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
